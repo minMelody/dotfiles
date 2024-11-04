52abd76008249ce29ec0e21ada02da63981698d3
minMelody <138899007+minMelody@users.noreply.github.com>
2024-11-04 20:56:32 +0100

    update

--- fish/functions/smallfetch.fish
+++ fish/functions/smallfetch.fish
@@ -1,4 +1,4 @@
-function smallfetch --wraps='fastfetch -l small --load-config compact.jsonc' --wraps='fastfetch -l small --load-config ~/.config/fastfetch/compact.jsonc' --description 'alias smallfetch fastfetch -l small --load-config ~/.config/fastfetch/compact.jsonc'
+function smallfetch --wraps='fastfetch -l small --load-config ~/.config/fastfetch/compact.jsonc' --description 'alias smallfetch fastfetch -l small --load-config ~/.config/fastfetch/compact.jsonc'
   fastfetch -l small --load-config ~/.config/fastfetch/compact.jsonc $argv
         
 end
